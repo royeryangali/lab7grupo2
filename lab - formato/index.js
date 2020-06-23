7f0c16224c2462c02f03b0a984a1b51fe9c31d62
Samuel Caro <a20170086@pucp.edu.pe>
2020-06-22 20:54:14 -0500

    Caro

--- lab - formato/index.js
+++ lab - formato/index.js
@@ -1,12 +1,67 @@
-
 $(document).ready(function () {
     // TODO, consultas a las web services
-});
 
-// Función para hacer el sort de un array
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: "https://api.covid19api.com/summary"
+    }).done(function (data) {
+
+        console.log(data);
+        $("#newConfirmed").html(data.Global.NewConfirmed);
+        $("#totalConfirmed").html(data.Global.TotalConfirmed);
+        $("#newDeaths").html(data.Global.NewDeaths);
+        $("#totalDeaths").html(data.Global.TotalDeaths);
+        $("#newRecovered").html(data.Global.NewRecovered);
+        $("#totalRecovered").html(data.Global.TotalRecovered);
+
+        var lista = data.Countries;
+        var contentHtml = "";
+        listaordenada = lista.sort(compare);
+        console.log(listaordenada);
+        $.each(listaordenada, function (i, country) {
+            var a = i + 1;
+            contentHtml += "<tr>";
+            contentHtml += "<td>" + a + "</td>";
+            contentHtml += "<td>" + country.Country + "</td>";
+            contentHtml += "<td>" + country.TotalConfirmed + "</td>";
+            contentHtml += "<td>" + country.TotalDeaths + "</td>";
+            contentHtml += "<td>" + country.TotalRecovered + "</td>";
+            contentHtml += "<td>" + country.NewConfirmed + "</td>";
+            contentHtml += "<td>" + country.NewDeaths + "</td>";
+            contentHtml += "<td>" + country.NewRecovered + "</td>";
+            contentHtml += "<td>" + "<a class='btn btn-primary btn-block' href='"+'detallePais/detallePais.html?name=' + country.Country +
+                '&slug=' + country.Slug + '&countryCode=' + country.CountryCode + "' role = 'button'>Ver Detalles </a>" + "</td >";
+            contentHtml += "<tr>";
+        })
+        $("#body-paises").html(contentHtml);
+
+
+    }).fail(function (err) {
+        var jsonData = err.responseJSON;
+        console.log(jsonData.msg);
+        alert(jsonData.msg);
+        return false;
+    })
+})
+;
+
+
 function compare(a, b) {
-    // TODO
+
+    const bandA = a.TotalConfirmed;
+    const bandB = b.TotalConfirmed;
+    let comparison = 0;
+    if (bandA < bandB) {
+        comparison = 1;
+    } else if (bandA > bandB) {
+        comparison = -1;
+    }
+    return comparison;
+
+
 }
+
 // Función para devolver un formato de fecha
 function formatDate(date) {
     var d = new Date(date),
@@ -21,3 +76,5 @@ function formatDate(date) {
     // TODO
     return '';
 }
+
+
